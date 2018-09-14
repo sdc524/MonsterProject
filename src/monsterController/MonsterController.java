bfcda703fc5c33fb593b21781be3910f5c5ccc0a
Stephen Cassat <Stephencassat@yahoo.com>
2018-09-14 08:07:24 -0600

    Added JOptionPane to project

--- src/monsterController/MonsterController.java
+++ src/monsterController/MonsterController.java
@@ -1,6 +1,9 @@
 package monsterController;
 
+import java.util.Scanner;
+
 import model.MarshmallowMonster;
+import javax.swing.JOptionPane;
 
 public class MonsterController
 {
@@ -12,16 +15,39 @@ public class MonsterController
 	public MonsterController()
 	{
 		//Job 1: intialize data members
-		myMonster = new MarshmallowMonster("Marshmallow", 2.0, 2, true, 2);
+		myMonster = new MarshmallowMonster("Marshmallow", 2.0, 40, true, 2);
+		
 	}
+	
 	
 	
 	
 	//methods section
 	public void start()
 	{
+		JOptionPane.showMessageDialog(null, "Show message here.");
+		//Use this method instead of "System.out.println("words");
+		
+		
+		
 		System.out.println(myMonster);
+		JOptionPane.showMessageDialog(null, myMonster);
 		myMonster.setArmCount(9999);
 		System.out.println("My monster has this may arms:" + myMonster.getArmCount());
+		JOptionPane.showMessageDialog(null, "My monster has 40 arms!");
+		//make a new monster and customize from user input!
+		
+		{
+		MarshmallowMonster userMonster;
+		
+		Scanner anotherInputScanner;
+		anotherInputScanner = new Scanner(System.in);
+		
+		System.out.println("What is your name?");
+		JOptionPane.showMessageDialog(null, "What is your name?");
+		int answer = anotherInputScanner.nextInt();
+		
+
+		}
 	}
 }
