4865be90dfbcbb3d0e8045a34fa268b5ff4ef300
Stephen Cassat <Stephencassat@yahoo.com>
2018-09-18 09:43:19 -0600

    Added if else statements

--- src/monsterController/MonsterController.java
+++ src/monsterController/MonsterController.java
@@ -28,26 +28,56 @@ public class MonsterController
 		JOptionPane.showMessageDialog(null, "Show message here.");
 		//Use this method instead of "System.out.println("words");
 		
-		
-		
-		System.out.println(myMonster);
+		//System.out.println(myMonster);
 		JOptionPane.showMessageDialog(null, myMonster);
 		myMonster.setArmCount(9999);
-		System.out.println("My monster has this may arms:" + myMonster.getArmCount());
-		JOptionPane.showMessageDialog(null, "My monster has 40 arms!");
+		JOptionPane.showMessageDialog(null,"My monster has this may arms:" + myMonster.getArmCount());
 		//make a new monster and customize from user input!
 		
+		JOptionPane.showMessageDialog(null, "Lets make you a monster!");
+		MarshmallowMonster userMonster = new MarshmallowMonster();
+		String userMonsterName = JOptionPane.showInputDialog(null, "What is your monster's name?");
+		String userNumber = JOptionPane.showInputDialog(null,"How many arms?");
+		if (validInt(userNumber))
 		{
-		MarshmallowMonster userMonster;
+			int arms = Integer.parseInt(userNumber);
+			userMonster.setArmCount(arms);
+		}
+	}
+	
+	public boolean validInt(String maybeInt)
+	{
+		boolean isValid = false;
 		
-		Scanner anotherInputScanner;
-		anotherInputScanner = new Scanner(System.in);
+		try 
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "You should type a integer value like -12345");
+		}
 		
-		System.out.println("What is your name?");
-		JOptionPane.showMessageDialog(null, "What is your name?");
-		int answer = anotherInputScanner.nextInt();
+		return isValid;
+	}
+
+	public boolean validDouble(String maybeDouble)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			Double.parseDouble(maybeDouble);
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "This requires a double value aka something with a . >)");
+		}
+		
+		return isValid;
+	}
 		
 
 		}
-	}
-}
+
